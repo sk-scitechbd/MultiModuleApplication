25fa13ceb9cc02b99f8b8c9feb5e4d2f89ca98e2
sk-scitechbd <sk.mishu@scitechbd.com>
2022-06-05 20:29:47 +0600

    Initial commit

---
+++ app/productsearchmodule/src/main/java/com/example/productsearchmodule/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.productsearchmodule
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.productsearchmodule.ui.theme.MultiModuleApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MultiModuleApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("from module search text")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MultiModuleApplicationTheme {
+        Greeting("Android")
+    }
+}
